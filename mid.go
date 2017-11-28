3d446e0b845dd74564430e4532fcbe6c44f7f823
Georgy Buranov <gburanov@gmail.com>
2017-11-28 21:11:06 +0100

    add go utils

---
+++ mid.go
@@ -0,0 +1,33 @@
+package goutils
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type cntFunc func(r *http.Request) error
+
+// HTTPError is error with error code
+type HTTPError struct {
+	Code    int
+	Message string
+}
+
+func (e *HTTPError) Error() string {
+	return fmt.Sprintf("Code %d, %s", e.Code, e.Message)
+}
+
+func HandleErrors(mid cntFunc) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := mid(r)
+		if err != nil {
+			if ae, ok := err.(*HTTPError); ok {
+				http.Error(w, ae.Error(), ae.Code)
+			} else {
+				http.Error(w, err.Error(), 500)
+			}
+			return
+		}
+		fmt.Fprintln(w, "OK")
+	}
+}
