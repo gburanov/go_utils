7fb61d71b227645094a8876dc5d59839567a4a6d
Georgy Buranov <gburanov@gmail.com>
2017-12-02 12:08:44 +0100

    more changes

--- coordinate.go
+++ coordinate.go
@@ -4,3 +4,8 @@ type Coordinate struct {
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 }
+
+type RedisCoordinate struct {
+	Coordinate
+	Seconds int64 `json:"seconds"`
+}
