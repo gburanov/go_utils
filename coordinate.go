de305ba4e6102921df4768cdd8b2b76220a02740
Georgy Buranov <gburanov@gmail.com>
2017-12-02 12:23:28 +0100

    1

--- coordinate.go
+++ coordinate.go
@@ -1,13 +1,14 @@
 package goutils
 
+import "time"
+
 type Coordinate struct {
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 }
 
 type RedisCoordinate struct {
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	Seconds   int64   `json:"seconds"`
-	Nanos     int32   `json:"nanos"`
+	Latitude  float64   `json:"latitude"`
+	Longitude float64   `json:"longitude"`
+	Time      time.Time `json:"updated_at"`
 }
